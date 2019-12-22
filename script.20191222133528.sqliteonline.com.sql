94c6d5316392c26954fa5044ef8f145106fc36e8
kaarrtt <59140453+kaarrtt@users.noreply.github.com>
2019-12-22 16:09:15 +0200

    Update script.20191222133528.sqliteonline.com.sql

--- script.20191222133528.sqliteonline.com.sql
+++ script.20191222133528.sqliteonline.com.sql
@@ -1,1 +1,1 @@
-select cat_name from (SELECT a.category_name as cat_name, avg(cost) as average from costs c inner join adverts a on a.advert_id = c.advert_id group by category_name) where average < '500'
+select cat_name from (SELECT a.category_name as cat_name, avg(cost) as average from costs c inner join adverts a on a.advert_id = c.advert_id group by category_name) as t where average < '500'
